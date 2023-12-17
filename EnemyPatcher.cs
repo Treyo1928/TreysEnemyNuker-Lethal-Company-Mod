66f3e8e239e7a7e61a1e45ec4a2ff0005d3de494
Trey <85683709+Treyo1928@users.noreply.github.com>
2023-12-17 15:36:47 -0600

    Added the .dll and the source code for the mod

---
+++ EnemyPatcher.cs
@@ -0,0 +1,94 @@
+﻿using HarmonyLib;
+
+namespace TreysEnemyNuker
+{
+	[HarmonyPatch(typeof(RoundManager), "LoadNewLevel")]
+	public static class EnemyManagerPatch
+	{
+		public static void Postfix(RoundManager __instance, int randomSeed, SelectableLevel newLevel)
+		{
+			var outsideEnemies = __instance.currentLevel.OutsideEnemies;
+			var Enemies = __instance.currentLevel.Enemies;
+			var daytimeEnemies = __instance.currentLevel.DaytimeEnemies;
+			if (outsideEnemies != null)
+			{
+				if (Plugin.RemoveMouthDog.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing MouthDogs");
+					outsideEnemies.RemoveAll(enemy => enemy.enemyType.enemyName == "MouthDog");
+				}
+				if (Plugin.RemoveForestGiant.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing ForestGiants");
+					outsideEnemies.RemoveAll(enemy => enemy.enemyType.enemyName == "ForestGiant");
+				}
+				if (Plugin.RemoveEarthLeviathan.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing EarthLeviathans");
+					outsideEnemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Earth Leviathan");
+				}
+				if (Plugin.RemoveBaboonHawk.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing BaboonHawks");
+					outsideEnemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Baboon hawk");
+				}
+			}
+			if (Enemies != null)
+			{
+				if (Plugin.RemoveCentipede.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Centipedes");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Centipede");
+				}
+				if (Plugin.RemoveBunkerSpider.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing BunkerSpiders");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Bunker Spider");
+				}
+				if (Plugin.RemoveHoardingBug.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing HoardingBugs");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Hoarding bug");
+				}
+				if (Plugin.RemoveFlowerMan.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing FlowerMen");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Flowerman");
+				}
+				if (Plugin.RemoveCrawler.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Crawlers");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Crawler");
+				}
+				if (Plugin.RemoveBlob.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Blobs");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Blob");
+				}
+				if (Plugin.RemoveSpring.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Springs");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Spring");
+				}
+				if (Plugin.RemovePuffer.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Puffers");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Puffer");
+				}
+				if (Plugin.RemoveNutcracker.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Nutcrackers");
+					Enemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Nutcracker");
+				}
+			}
+			if (daytimeEnemies != null)
+			{
+				if (Plugin.RemoveManticoil.Value)
+				{
+					Plugin.PluginLogger.LogInfo("Tried removing Manticoils");
+					daytimeEnemies.RemoveAll(enemy => enemy.enemyType.enemyName == "Manticoil");
+				}
+			}
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace TreysEnemyNuker
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> RemoveMouthDog;
+        public static ConfigEntry<bool> RemoveForestGiant;
+        public static ConfigEntry<bool> RemoveEarthLeviathan;
+        public static ConfigEntry<bool> RemoveBaboonHawk;
+        public static ConfigEntry<bool> RemoveCentipede;
+        public static ConfigEntry<bool> RemoveBunkerSpider;
+        public static ConfigEntry<bool> RemoveHoardingBug;
+        public static ConfigEntry<bool> RemoveFlowerMan;
+        public static ConfigEntry<bool> RemoveCrawler;
+        public static ConfigEntry<bool> RemoveBlob;
+        public static ConfigEntry<bool> RemoveSpring;
+        public static ConfigEntry<bool> RemovePuffer;
+        public static ConfigEntry<bool> RemoveNutcracker;
+        public static ConfigEntry<bool> RemoveManticoil;
+
+        internal static BepInEx.Logging.ManualLogSource PluginLogger;
+
+        private void Awake()
+        {
+            RemoveMouthDog = Config.Bind("Enemy Removal", "MouthDog", true, "Set to true to remove Mouth Dogs from the game.");
+            RemoveForestGiant = Config.Bind("Enemy Removal", "ForestGiant", true, "Set to true to remove Forest Giants from the game.");
+            RemoveEarthLeviathan = Config.Bind("Enemy Removal", "EarthLeviathan", true, "Set to true to remove Earth Leviathans from the game.");
+            RemoveBaboonHawk = Config.Bind("Enemy Removal", "BaboonHawk", true, "Set to true to remove Baboon Hawks from the game.");
+            RemoveCentipede = Config.Bind("Enemy Removal", "Centipede", true, "Set to true to remove Centipedes from the game.");
+            RemoveBunkerSpider = Config.Bind("Enemy Removal", "BunkerSpider", true, "Set to true to remove Bunker Spiders from the game.");
+            RemoveHoardingBug = Config.Bind("Enemy Removal", "HoardingBug", true, "Set to true to remove Hoarding Bugs from the game.");
+            RemoveFlowerMan = Config.Bind("EnemyRemoval", "FlowerMan", true, "Set to true to remove Flower Man from the game.");
+            RemoveCrawler = Config.Bind("EnemyRemoval", "Crawler", true, "Set to true to remove Crawlers from the game.");
+            RemoveBlob = Config.Bind("EnemyRemoval", "Blob", true, "Set to true to remove Blobs from the game.");
+            RemoveSpring = Config.Bind("EnemyRemoval", "Spring", true, "Set to true to remove Springs from the game.");
+            RemovePuffer = Config.Bind("EnemyRemoval", "Puffer", true, "Set to true to remove Puffers from the game.");
+            RemoveNutcracker = Config.Bind("EnemyRemoval", "Nutcracker", true, "Set to true to remove Nutcrackers from the game.");
+            RemoveManticoil = Config.Bind("EnemyRemoval", "Manticoil", true, "Set to true to remove Manticoils from the game.");
+
+            PluginLogger = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            // Initialize Harmony
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            // Log that patching process has been initiated
+            Logger.LogInfo("Harmony patches applied.");
+        }
+    }
+}
